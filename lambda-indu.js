12a4c5a48aaa00acb0d521679e7c3fbac015f03f
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 15:05:18 +0530

    platform Application

--- lambda-indu.js
+++ lambda-indu.js
@@ -2,25 +2,18 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
+
     try {
-        let data = await sns.publish({
-            Message: "hi test 2/27/2020",
-            PhoneNumber: "+94713245242",
-            MessageAttributes: {
-                'AWS.SNS.SMS.SMSType': {
-                    DataType: "String",
-                    StringValue: "Transactional"
-                }
-            }
+        let data = await sns.deleteEndpoint({
+            EndpointArn: "arn:aws:sns:us-east-1:318300609668:endpoint/MPNS/snsAppTestCafe/089788cf-baa2-4b7b-a864-4490da4a2ae4"
         }).promise();
+                console.log("data");
+                console.log(data);
 
-
-        console.log("data");
-        console.log(data);
     } catch (err) {
         // error handling goes here
-        console.log("err");
-        console.log(err);
+                console.log("err");
+                console.log(err);
 
     };
 
