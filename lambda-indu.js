268bceeb52da6adf0bfd39df9f34f60b25e83370
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 15:39:25 +0530

    test1

--- lambda-indu.js
+++ lambda-indu.js
@@ -3,19 +3,20 @@ const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
     try {
-        let data = await sns.publish({
-            Message: "hi",
-            PhoneNumber: "+94713245242",
-            MessageAttributes: {
-                'AWS.SNS.SMS.SMSType': {
-                    DataType: "String",
-                    StringValue: "Promotional"
-                }
-            }
+        let data = await sns.createPlatformEndpoint({
+            PlatformApplicationArn: "arn:aws:sns:us-east-1:318300609668:app/MPNS/snsAppTestCafe",
+            Token: "135081n",
+            Attributes: {}
         }).promise();
 
+
+        console.log("data");
+                console.log(data);
     } catch (err) {
         // error handling goes here
+                console.log("err");
+                console.log(err);
+
     };
 
     return { "message": "Successfully executed: sns" };
