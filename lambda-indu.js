643ca111bb16af6f8239892a951be607302b4534
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 13:26:29 +0530

    top attribute

--- lambda-indu.js
+++ lambda-indu.js
@@ -3,21 +3,20 @@ const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
     try {
-        let data = await sns.publish({
-            Message: "hi, test ",
-            Subject: "test sns",
-            TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
-            MessageStructure: "String",
-            MessageAttributes: {}
+        let data = await sns.getTopicAttributes({
+            TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS"
         }).promise();
+
         console.log("data");
                 console.log(data);
+                
     } catch (err) {
         // error handling goes here
                 console.log("err");
                 console.log(err);
-    };
 
+        
+    };
 
     return { "message": "Successfully executed: 123" };
 };
