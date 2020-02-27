326a7cfb76f42312342927a829d35e41a49cd9bf
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 13:53:49 +0530

    1

--- lambda-indu.js
+++ lambda-indu.js
@@ -5,8 +5,8 @@ exports.handler = async (event) => {
     try {
         let data = await sns.subscribe({
             TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
-            Protocol: "sms",
-            Endpoint: "94713245242"
+            Protocol: "https",
+            Endpoint: "https://www.slappforge.com/docs/indu"
         }).promise();
 
     } catch (err) {
