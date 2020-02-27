46433a72e841e782120c59ba2e30965afaad3295
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 14:33:04 +0530

    test 1

--- lambda-indu.js
+++ lambda-indu.js
@@ -4,17 +4,16 @@ const sns = new AWS.SNS();
 exports.handler = async (event) => {
     try {
         let data = await sns.unsubscribe({
-            SubscriptionArn: "arn:aws:sns:us-east-1:318300609668:TestSNS:5aac42c6-3953-48ac-977e-d7d3ed5504be	"
+            SubscriptionArn: "arn:aws:sns:us-east-1:318300609668:TestSNS:5aac42c6-3953-48ac-977e-d7d3ed5504be"
         }).promise();
-
         console.log("data");
                 console.log(data);
     } catch (err) {
         // error handling goes here
-    };
-            console.log("err");
+                console.log("err");
                 console.log(err);
 
+    };
 
     return { "message": "Successfully executed: test" };
 };
