2cb991b4f02e9ed50b46823de937f14b0208f093
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 13:18:10 +0530

    dep sns op 1 : pub msg

--- lambda.js
+++ lambda.js
@@ -1,4 +1,22 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    try {
+        let data = await sns.publish({
+            Message: "hi, test ",
+            Subject: "test sns",
+            TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
+            MessageStructure: "String",
+            MessageAttributes: {}
+        }).promise();
+        console.log("data");
+                console.log(data);
+    } catch (err) {
+        // error handling goes here
+    };
+        console.log("err");
+                console.log(err);
+
+    return { "message": "Successfully executed" };
 };
