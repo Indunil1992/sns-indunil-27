52d65813b3ae5dcc02dd1e42434f2919c5d40371
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 15:41:34 +0530

    1

--- lambda-indu.js
+++ lambda-indu.js
@@ -5,17 +5,17 @@ exports.handler = async (event) => {
     try {
         let data = await sns.createPlatformEndpoint({
             PlatformApplicationArn: "arn:aws:sns:us-east-1:318300609668:app/MPNS/snsAppTestCafe",
-            Token: "135081n",
+            Token: "135081135081",
             Attributes: {}
         }).promise();
 
 
         console.log("data");
-                console.log(data);
+        console.log(data);
     } catch (err) {
         // error handling goes here
-                console.log("err");
-                console.log(err);
+        console.log("err");
+        console.log(err);
 
     };
 
