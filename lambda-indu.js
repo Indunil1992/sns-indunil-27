be2b4532a93577f1610d94f56aaf200d399ddaed
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 13:30:39 +0530

    listSubscription

--- lambda-indu.js
+++ lambda-indu.js
@@ -2,20 +2,19 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
+
     try {
-        let data = await sns.getTopicAttributes({
+        let data = await sns.listSubscriptionsByTopic({
             TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS"
         }).promise();
 
         console.log("data");
                 console.log(data);
-
     } catch (err) {
         // error handling goes here
                 console.log("err");
                 console.log(err);
 
-        
     };
 
     return { "message": "Successfully executed: test" };
