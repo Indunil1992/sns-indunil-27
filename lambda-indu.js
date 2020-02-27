9bdac49858078bdd4e26d25b7e83421d584548b7
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 13:48:38 +0530

    sqs sub

--- lambda-indu.js
+++ lambda-indu.js
@@ -5,16 +5,12 @@ exports.handler = async (event) => {
     try {
         let data = await sns.subscribe({
             TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
-            Protocol: "email",
-            Endpoint: "sachithrarajapakse1992@gmail.com"
+            Protocol: "sqs",
+            Endpoint: "arn:aws:sqs:us-east-1:318300609668:indunil-message-queue"
         }).promise();
-        console.log("data");
-                console.log(data);
+
     } catch (err) {
         // error handling goes here
-                console.log("err");
-                console.log(err);
-
     };
 
     return { "message": "Successfully executed: test" };
