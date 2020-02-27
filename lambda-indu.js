4a26818f1eb49f04bfb3b44f68a781c33589909c
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 13:40:49 +0530

    subscribe

--- lambda-indu.js
+++ lambda-indu.js
@@ -3,12 +3,12 @@ const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
     try {
-        let data = await sns.setTopicAttributes({
+        let data = await sns.subscribe({
             TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
-            AttributeName: "DisplayName",
-            AttributeValue: "test indunil-27/2/2020"
+            Protocol: "email",
+            Endpoint: "sachithrarajapakse1992@gmail.com"
         }).promise();
-      console.log("data");
+        console.log("data");
                 console.log(data);
     } catch (err) {
         // error handling goes here
