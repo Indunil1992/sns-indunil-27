1acbf6a6f677421c8670985f2a88915249c8b6d0
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 15:22:23 +0530

    create 1

--- lambda-indu.js
+++ lambda-indu.js
@@ -2,20 +2,21 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
-
     try {
-        let data = await sns.deleteEndpoint({
-            EndpointArn: "arn:aws:sns:us-east-1:318300609668:endpoint/MPNS/snsAppTestCafe/089788cf-baa2-4b7b-a864-4490da4a2ae4"
+        let data = await sns.createPlatformEndpoint({
+            PlatformApplicationArn: "arn:aws:sns:us-east-1:318300609668:app/MPNS/snsAppTestCafe",
+            Token: "123123",
+            Attributes: {}
         }).promise();
-                console.log("data");
+        console.log("data");
                 console.log(data);
-
     } catch (err) {
         // error handling goes here
                 console.log("err");
                 console.log(err);
 
     };
+
 
     return { "message": "Successfully executed: sns" };
 };
